c924ebc524849f772d73a304ae7786f44426785b
Joseph LaMantia Jr <40052124+JosephLaMantia@users.noreply.github.com>
2020-11-13 18:44:49 -0500

    Update PokemonRunner.java
    Condensed the runner file into a more readable format, converting the code into methods that I may implement more than once in future edits.

--- PokemonLab/src/PokemonRunner.java
+++ PokemonLab/src/PokemonRunner.java
@@ -10,18 +10,41 @@ public class PokemonRunner {
 		HumanPlayer player = new HumanPlayer();
 		ComputerPlayer npc = new ComputerPlayer();
 		
-		//welcome
+		welcome(player, npc);	// Welcome message, introducing the user, announcing their pokemon, item, and their npc opponent.
+		
+		System.out.println("You go first!\n");	// Battle begins with Players turn
+		
+		battle(player, npc);	
+	}
+	
+	/**
+	 * Introduces user to the program, explaining the controls.
+	 * Informs player (user) what item and pokemon they have been equipped with.
+	 * Uses the players pokemons .speak() method
+	 * Informs player what pokemon their npc opponent has been equipped with. (Does not reveal which item.)
+	 * Uses the npc pokemons .speak() method
+	 * Announces the battle is starting
+	 * @param human Player instance variable
+	 * @param computer Player instance variable
+	 */
+	private static void welcome(Player human, Player computer) {
 		System.out.println("Hi! Welcome to the Pokemon battle simulator. Make sure to use your number keys to control the action!\n");
-		System.out.println("You have been equipped with one Power Potion.\nYour new pokemon, " + player.getPokemon().getPokemonName() + " would like to say hello!");
-		player.getPokemon().speak();
+		System.out.println("You have been equipped with one Power Potion.\nYour new pokemon, " + human.getPokemon().getPokemonName() + " would like to say hello!");
+		human.getPokemon().speak();
 		System.out.println("");
-		System.out.println("You will be battling your opponents pokemon, " + npc.getPokemon().getPokemonName() + "!");
-		npc.getPokemon().speak();
+		System.out.println("You will be battling your opponents pokemon, " + computer.getPokemon().getPokemonName() + "!");
+		computer.getPokemon().speak();
 		System.out.println("\n Let the battle begin! \n ... \n");
 		
-		player.getPokemon().status(npc.getPokemon());;
-		System.out.println("You go first!\n");
-		
+		human.getPokemon().status(computer.getPokemon());
+	}
+	
+	/**
+	 * Uses the Player.act method in a turn-based order, checking whether or not the game has ended with each turn.
+	 * @param player Player instance variable
+	 * @param npc Player instance variable
+	 */
+	private static void battle(HumanPlayer player, ComputerPlayer npc) {
 		while(player.getGameOver() == false && npc.getGameOver() == false) {
 			if(player.getGameOver() == false && npc.getGameOver() == false) {
 				player.act(npc);
@@ -32,3 +55,4 @@ public class PokemonRunner {
 		}	
 	}
 }
+
